1992b920078a3da58ea842b37522af3158fc5da1
ryan605 <langatryan26@gmail.com>
2022-11-29 09:17:56 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function receivesAFunction(callback){
+    return callback()
+}
+function returnsANamedFunction(){
+    return returnsANamedFunction
+}
+function returnsAnAnonymousFunction(){
+    return ()=>fn
+}
